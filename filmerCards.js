9046644c61b0a72bc40fadbd3e001d9a13ebf0f2
Mats Johansson <matjoh50@gmail.com>
2023-12-28 17:26:52 +0100

    Created the filmer.scss and more value in the js

--- filmerCards.js
+++ filmerCards.js
@@ -1,27 +1,32 @@
 function renderFilmerCards(movieObject) {
     const baseCard = document.createElement("div");
+    baseCard.classList.add("filmer__card")
     //Image in this space
 
     const baseCardInfoBox = document.createElement("div"); //Wrapper for the info
-
+    baseCardInfoBox.classList.add("filmer__card--wrapper")
 
     const infoBoxTitle = document.createElement("div");
-    const movieTitle = document.createTextNode(movieObject, "title");
+    const movieTitle = document.createTextNode(movieObject + " title");
+    infoBoxTitle.classList.add("filmer__card--title")
     infoBoxTitle.appendChild(movieTitle);
     baseCardInfoBox.appendChild(infoBoxTitle)
     
     const infoBoxLength = document.createElement("div");
-    const moviePlayTime = document.createTextNode(movieObject,+ "length");
+    const moviePlayTime = document.createTextNode(movieObject + " length");
+    infoBoxLength.classList.add("filmer__card--length")
     infoBoxLength.appendChild(moviePlayTime);
     baseCardInfoBox.appendChild(infoBoxLength)
     
     const infoBoxDescription = document.createElement("div");
-    const movieDescription = document.createTextNode(movieObject, "description");
+    const movieDescription = document.createTextNode(movieObject + " description");
+    infoBoxDescription.classList.add("filmer__card--description")
     infoBoxDescription.appendChild(movieDescription);
     baseCardInfoBox.appendChild(infoBoxDescription)
     
     const infoBoxBooking = document.createElement("div");
     const bookingButton = document.createTextNode("Boka här");
+    infoBoxBooking.classList.add("filmer__card--button")
     infoBoxBooking.appendChild(bookingButton);
     baseCardInfoBox.appendChild(infoBoxBooking)
 
