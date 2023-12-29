5c4197dcef77f27991ac108de01cd4a43c0d5852
AnnaLenaO <annalenao@me.com>
2023-12-29 15:08:58 +0100

    skeleton templates & js

--- aboutUs.js
+++ aboutUs.js
@@ -1,16 +1,20 @@
 //template elements
-let headerTemplate = document.getElementById("header");
-
-let footerTemplate = document.getElementById("footer");
-
+let headerTemplate = document.querySelector("#header");
+let heroSectionTemplate = document.querySelector("#heroSection");
+let startedSectionTemplate = document.querySelector("#startedSection");
+let sponsorSectionTemplate = document.querySelector("#sponsorSection");
+let footerTemplate = document.querySelector("#footer");
 
 //clone templates content
-let headerClone = template.content.cloneNode(true);
-
-let footerClone = template.content.cloneNode(true);
-
+let headerClone = headerTemplate.content.cloneNode(true);
+let heroSectionClone = heroSectionTemplate.content.cloneNode(true);
+let startedSectionClone = startedSectionTemplate.content.cloneNode(true);
+let sponsorSectionClone =sponsorSectionTemplate.content.cloneNode(true);
+let footerClone = footerTemplate.content.cloneNode(true);
 
 //append clones to body
 document.body.appendChild(headerClone);
-
+document.body.appendChild(heroSectionClone);
+document.body.appendChild(startedSectionClone);
+document.body.appendChild(sponsorSectionClone);
 document.body.appendChild(footerClone);
